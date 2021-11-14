3c8d167405f5e17c513666cbbd65451a72817c82
umit karabaxak <umit.karabacak@hotmail.com.tr>
2021-11-15 02:02:55 +0300

    validate correlationId in request & response

--- SampleA/Controllers/WeatherForecastController.cs
+++ SampleA/Controllers/WeatherForecastController.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SampleA.Controllers
@@ -24,16 +26,40 @@ namespace SampleA.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+
+            var beforeRequestHeaderKeys = HttpContext.Request.Headers.Select(i => i.Key).ToList();
+            var beforeResponseHeaderKeys = HttpContext.Response.Headers.Select(i => i.Key).ToList();
+
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://localhost:6001"),
+            };
+            var response = await client.GetAsync("weatherforecast");
+
+            var afterRequestHeaderKeys = HttpContext.Request.Headers.Select(i => i.Key).ToList();
+            var afterResponseHeaderKeys = HttpContext.Response.Headers.Select(i => i.Key).ToList();
+
+            var responseData = new
+            {
+                beforeRequestHeaderKeys,
+                beforeResponseHeaderKeys,
+                afterRequestHeaderKeys,
+                afterResponseHeaderKeys,
+            };
+
+            _logger.LogInformation(JsonSerializer.Serialize(responseData));
+
+            return result;
         }
     }
 }
