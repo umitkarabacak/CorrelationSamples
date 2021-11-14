c989d324089adbbded8dc46635010651a49d581c
umit karabaxak <umit.karabacak@hotmail.com.tr>
2021-11-15 01:47:28 +0300

    default correlationId nuget add & test

--- SampleC/Startup.cs
+++ SampleC/Startup.cs
@@ -1,15 +1,10 @@
+using CorrelationId;
+using CorrelationId.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SampleC
 {
@@ -26,6 +21,8 @@ namespace SampleC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDefaultCorrelationId();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +32,8 @@ namespace SampleC
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCorrelationId();
 
             app.UseHttpsRedirection();
 
